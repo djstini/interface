21f9e263f37fdcc2413879a51fe657c1e9b88bdd
dennisstinauer <dennis@stinauer.net>
2024-03-10 12:34:48 +0100

    fix(rest): json formatting

--- rest.php
+++ rest.php
@@ -4,7 +4,7 @@ header('Content-Type: application/json; charset=utf-8');
 
 
 $output_disk = array();
-exec('df -h | grep dev/md127 | awk \'{print "{size:  \""$2"\", used: \""$3"\", available: \""$4"\", percentage: \""$5"\", },"}\'', $output_disk);
+exec('df -h | grep dev/md127 | awk \'{print "{\"size\":  \""$2"\", \"used\": \""$3"\", \"available\": \""$4"\", \"percentage\": \""$5"\", },"}\'', $output_disk);
 
 $output_top = array();
 exec('mpstat -u -P ALL 1 1 | awk \'
@@ -14,25 +14,34 @@ exec('mpstat -u -P ALL 1 1 | awk \'
 	NR > 20 {next}
         {sum[$2] += $3; sum[$2] += $4; sum[$2] += $5;}
 	END { for (i in sum) {
-            print "{ core: \""i"\", usage: \""sum[i]"\", },"}
+            print "{ \"core\": \""i"\", \"usage\": \""sum[i]"\", },"}
         }
 \' | sort -nk1', $output_top);
 
 $output_temp = array();
-exec('sensors | grep -E "Core|Sensor" | awk \'{print "{ core: \""$1" "$2"\", temp: \""$3"\", },"} \'', $output_temp);
+exec('sensors | grep -E "Core|Sensor" | awk \'{print "{ \"core\": \""$1" "$2"\", \"temp\": \""$3"\", },"} \'', $output_temp);
 
 ?>
-{ disk: [ <?php
-foreach($output_disk as $line){ 
-    echo $line;
+{ 
+"disk": [
+    <?php
+    foreach($output_disk as $line){ 
+        echo $line;
+    }
+    ?> 
+],
+"top": [
+    <?php
+    foreach($output_top as $line){ 
+            echo $line;
+        }
+    ?>
+],
+"temps": [
+    <?php
+    foreach($output_temp as $line){ 
+            echo $line;
+        }
+    ?>
+],
 }
-?> ], top: [ <?php
-foreach($output_top as $line){ 
-        echo $line;
-    }
-?> ], temps: [
-<?php
-foreach($output_temp as $line){ 
-        echo $line;
-    }
-?> ],}
